5d06eac09c8c0dc6372d888184fb1712f20044ef
harukanoo <harukanox2@gmail.com>
2018-10-19 18:16:19 +0700

    make a first connection

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('hello world')
+const express = require('express');
+const logger= require('morgan');
+const apps = express();
+
+
+        apps.use(logger('dev'));
+
+        apps.get('/',(req,res,next)=>{
+            res.status(200).json({
+                message:"you request a page"
+            })
+        });
+
+        apps.use((req,res,next)=>{
+            const err = new Error('Not Found');
+            err.status =404;
+            next(err);
+        });
+
+        apps.use((err,req,res,next)=>{
+            const error = apps.get('env')==='development'? err:{};
+            const status = err.status || 500;
+            
+            res.status(status).json({
+                error:{
+                    message: error.message
+                }
+            })
+        });
+
+
+module.exports =apps;
